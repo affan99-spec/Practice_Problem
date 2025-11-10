f4a3196c291f6b8c85be9e4873454edb4eca8006
affan99-spec <affan.25005019@kiet.edu>
2025-11-10 22:44:02 +0530

    Add files via upload

---
+++ prob9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<=7; i++) {
+        for(int j=1; j<=7; j++) {
+            if(j<=8-i && j>=i) {
+                cout<<"* ";
+            } else if(j>=8-i && j<=i) {
+                cout<<"* ";
+            } else {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
